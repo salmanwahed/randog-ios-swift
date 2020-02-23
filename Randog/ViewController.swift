285621d800d85a83364de4f88b991f1c27e97836
Salman Wahed <dev.salmanwahed@gmail.com>
2020-02-23 17:15:56 +0600

    Initial Commit

---
+++ Randog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Randog
+//
+//  Created by Salman Wahed on 2/23/20.
+//  Copyright © 2020 Mac Mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
