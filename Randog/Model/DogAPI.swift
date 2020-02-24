0cbe8c915276ccc023ad288bba0ede23b9995ccf
Salman Wahed <dev.salmanwahed@gmail.com>
2020-02-24 12:36:18 +0600

    Added gitignore file for ios-swift project

---
+++ Randog/Model/DogAPI.swift
@@ -0,0 +1,9 @@
+//
+//  DogAPI.swift
+//  Randog
+//
+//  Created by Salman Wahed on 2/23/20.
+//  Copyright © 2020 Mac Mini. All rights reserved.
+//
+
+import Foundation
---
+++ Randog/Model/DogImage.swift
@@ -0,0 +1,9 @@
+//
+//  DogImage.swift
+//  Randog
+//
+//  Created by Salman Wahed on 2/23/20.
+//  Copyright © 2020 Mac Mini. All rights reserved.
+//
+
+import Foundation
